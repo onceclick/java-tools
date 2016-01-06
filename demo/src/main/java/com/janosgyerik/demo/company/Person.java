f8cbda66699a970ec60a370e94092715219d10f0
Janos Gyerik <janos@kronos>
2016-01-06 23:44:43 +0100

    added demo project with CsvWriterDemo

---
+++ demo/src/main/java/com/janosgyerik/demo/company/Person.java
@@ -0,0 +1,16 @@
+package com.janosgyerik.demo.company;
+
+public class Person {
+    public String name;
+    public int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%d)", name, age);
+    }
+}
---
+++ demo/src/main/java/com/janosgyerik/demo/csv/writing/CsvWriterDemo.java
@@ -0,0 +1,82 @@
+package com.janosgyerik.demo.csv.writing;
+
+import com.janosgyerik.demo.company.Person;
+import com.janosgyerik.tools.files.csv.writing.Columnizer;
+import com.janosgyerik.tools.files.csv.writing.CsvWriter;
+import com.janosgyerik.tools.files.csv.writing.CsvWriters;
+import com.janosgyerik.tools.files.csv.writing.ObjectWriter;
+import com.janosgyerik.tools.util.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class CsvWriterDemo {
+
+    List<Person> persons = Arrays.asList(new Person("Jack", 25), new Person("Mike", 29));
+
+    Columnizer<Person> columnizer = new Columnizer<Person>() {
+        @Override
+        public Object[] getHeaders() {
+            return new Object[]{"name", "age"};
+        }
+
+        @Override
+        public Object[] getValues(Person item) {
+            return new Object[]{item.name, item.age};
+        }
+    };
+
+    public static void main(String[] args) throws IOException {
+        CsvWriterDemo demo = new CsvWriterDemo();
+        demo.writeColumnsDemo();
+        demo.writeObjectsDemo();
+        demo.writeToFileDemo();
+        demo.writeToBufferDemo();
+        demo.writeCollectionDemo();
+    }
+
+    private void writeColumnsDemo() throws IOException {
+        printHeader("writeColumnsDemo");
+        CsvWriter writer = CsvWriters.builder().columnsWriter();
+        writer.writeLine("name", "age");
+        Person person = persons.get(0);
+        writer.writeLine(person.name, person.age);
+    }
+
+    private void writeObjectsDemo() throws IOException {
+        printHeader("writeObjectsDemo");
+        ObjectWriter<Person> writer = CsvWriters.builder().objectWriter(columnizer);
+        writer.writeHeader();
+        writer.writeObject(persons.get(0));
+    }
+
+    private void writeToBufferDemo() throws IOException {
+        printHeader("writeToBufferDemo");
+
+        StringWriter writer = new StringWriter();
+        CsvWriters.builder().writer(writer).write(persons, columnizer);
+
+        System.out.println(writer.toString());
+    }
+
+    private void writeToFileDemo() throws IOException {
+        printHeader("writeToFileDemo");
+
+        String path = FileUtils.createTempFile().getPath();
+        CsvWriters.builder().path(path).write(persons, columnizer);
+
+        System.out.println(FileUtils.read(new File(path)));
+    }
+
+    private void writeCollectionDemo() throws IOException {
+        printHeader("writeCollectionDemo");
+        CsvWriters.builder().write(persons, columnizer);
+    }
+
+    private void printHeader(String label) {
+        System.out.println("---- " + label + " ----");
+    }
+}
