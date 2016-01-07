5e1fa73a2b7e3747bdb59dfe8e1fefe28cda8be3
Janos Gyerik <janos@kronos>
2016-01-07 23:27:20 +0100

    added javadoc for the methods of BalancedTreeUtils

--- tools/src/main/java/com/janosgyerik/tools/algorithm/tree/binarytree/balanced/BalancedTreeUtils.java
+++ tools/src/main/java/com/janosgyerik/tools/algorithm/tree/binarytree/balanced/BalancedTreeUtils.java
@@ -10,10 +10,27 @@ public class BalancedTreeUtils {
         // utility class, forbidden constructor
     }
 
+    /**
+     * Check if a binary tree is height-balanced:
+     *
+     * 1. The heights of the left and right sub-trees differ by at most 1
+     * 2. The left sub-tree is balanced
+     * 3. The rightsub-tree is balanced
+     *
+     * @param root the root node of the tree
+     * @return true if the tree is balanced
+     */
     public static boolean isBalanced(TreeNode<?> root) {
         return balancedHeight(root) != UNBALANCED;
     }
 
+    /**
+     * Find the height of a balanced binary tree.
+     * If the tree is not balanced, return the constant UNBALANCED.
+     *
+     * @param root the root node of the tree
+     * @return the height if the tree is balanced, otherwise the constant UNBALANCED
+     */
     private static int balancedHeight(TreeNode<?> root) {
         if (root == null) {
             return 0;
