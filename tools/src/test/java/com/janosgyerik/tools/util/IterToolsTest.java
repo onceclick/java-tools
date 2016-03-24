3d13c0d67861a7834e6ba75af290ccc5787ddcb6
Janos Gyerik <janos@kronos>
2016-03-24 22:38:45 +0100

    more tests for permutation iterator

--- tools/src/test/java/com/janosgyerik/tools/util/IterToolsTest.java
+++ tools/src/test/java/com/janosgyerik/tools/util/IterToolsTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.*;
 
+import static com.janosgyerik.tools.util.IterTools.toList;
 import static org.junit.Assert.assertEquals;
 
 public class IterToolsTest {
@@ -49,17 +50,46 @@ public class IterToolsTest {
     @Test
     public void should_get_equal_list_from_iterator() {
         List<Integer> list = Arrays.asList(3, 1, 4, 5, 2);
-        assertEquals(list, IterTools.toList(list.iterator()));
+        assertEquals(list, toList(list.iterator()));
     }
 
     @Test
     public void should_get_empty_list_from_empty_iterator() {
         List<Integer> list = Collections.emptyList();
-        assertEquals(list, IterTools.toList(list.iterator()));
+        assertEquals(list, toList(list.iterator()));
     }
 
     @Test
-    public void test_permutationGenerator_a_b_c() {
-        Iterator<List<Character>> it = IterTools.permutationIterator(Arrays.asList('a', 'b', 'c'));
+    public void should_get_1_empty_permutation_for_empty_list() {
+        List<List<Integer>> permutations = toList(IterTools.permutationIterator(Collections.emptyList()));
+        assertEquals(Collections.singletonList(Collections.emptyList()), permutations);
+    }
+
+    @Test
+    public void should_get_1_permutation_for_singleton_list() {
+        List<List<Integer>> permutations = toList(IterTools.permutationIterator(Collections.singletonList(7)));
+        assertEquals(Collections.singletonList(Collections.singletonList(7)), permutations);
+    }
+
+    @Test
+    public void should_get_2_ordered_permutations_for_x_y() {
+        List<List<Character>> permutations = toList(IterTools.permutationIterator(Arrays.asList('x', 'y')));
+        assertEquals(Arrays.asList(
+                Arrays.asList('x', 'y'),
+                Arrays.asList('y', 'x')
+        ), permutations);
+    }
+
+    @Test
+    public void should_get_6_ordered_permutations_for_1_2_3() {
+        List<List<Integer>> permutations = toList(IterTools.permutationIterator(Arrays.asList(1, 2, 3)));
+        assertEquals(Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(1, 3, 2),
+                Arrays.asList(2, 1, 3),
+                Arrays.asList(2, 3, 1),
+                Arrays.asList(3, 1, 2),
+                Arrays.asList(3, 2, 1)
+        ), permutations);
     }
 }
