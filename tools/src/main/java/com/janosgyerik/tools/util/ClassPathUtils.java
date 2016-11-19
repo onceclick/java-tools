7deb28f9c7b0ff714fc382ba48d3b6c1633b22d7
Janos Gyerik <janos@kronos>
2016-11-19 07:17:06 +0100

    Document public methods

--- tools/src/main/java/com/janosgyerik/tools/util/ClassPathUtils.java
+++ tools/src/main/java/com/janosgyerik/tools/util/ClassPathUtils.java
@@ -2,20 +2,43 @@ package com.janosgyerik.tools.util;
 
 import java.net.URL;
 
+/**
+ * Utility methods related to classpath.
+ */
 public final class ClassPathUtils {
 
     private ClassPathUtils() {
         throw new AssertionError("utility class, forbidden constructor");
     }
 
+    /**
+     * Get the URL of a resource by name.
+     *
+     * @param resourceName name of the resource to find
+     * @return URL of the resource
+     */
     public URL getResourceUrl(String resourceName) {
         return getClass().getClassLoader().getResource(resourceName);
     }
 
+    /**
+     * Get the URL of the JAR from which a class was loaded.
+     *
+     * @param clazz the class to find
+     * @return the URL of the class
+     */
     public static URL getJarUrl(Class<?> clazz) {
         return clazz.getProtectionDomain().getCodeSource().getLocation();
     }
 
+    /**
+     * Get the URL of the JAR from which a class was loaded.
+     * This can be useful when the Class instance is not available directly in the caller,
+     * for example because it would be loaded further down the stack.
+     *
+     * @param className the name of the class to find
+     * @return the URL of the class
+     */
     public static URL getJarUrl(String className) {
         try {
             return getJarUrl(Class.forName(className));
@@ -24,6 +47,12 @@ public final class ClassPathUtils {
         }
     }
 
+    /**
+     * Get the URL of the class file from which a class was loaded.
+     *
+     * @param clazz the class to find
+     * @return the URL of the class
+     */
     public static URL getClassUrl(Class<?> clazz) {
         return clazz.getResource("/" + clazz.getName().replace(".", "/") + ".class");
     }
