0d964d34a10c0bb809c3eafc1b13ffe40d8ccfac
Janos Gyerik <janos@kronos>
2016-11-23 20:03:25 +0100

    Resolve violation

--- tools/src/main/java/com/janosgyerik/tools/files/demo/HelloWorld.java
+++ tools/src/main/java/com/janosgyerik/tools/files/demo/HelloWorld.java
@@ -1,7 +1,11 @@
 package com.janosgyerik.tools.files.demo;
 
 public class HelloWorld {
+    private HelloWorld() {
+
+    }
+
     public static void main(String[] args) {
-        // done!
+        // happy now?
     }
 }
