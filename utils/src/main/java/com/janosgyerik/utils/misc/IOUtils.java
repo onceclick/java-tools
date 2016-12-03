7ddc691a3f8e3de31c166603814357d8c57d08f2
Janos Gyerik <janos@kronos>
2016-12-03 23:33:25 +0100

    Do not reuse method parameter as local var

--- utils/src/main/java/com/janosgyerik/utils/misc/IOUtils.java
+++ utils/src/main/java/com/janosgyerik/utils/misc/IOUtils.java
@@ -16,24 +16,26 @@ public class IOUtils {
 
     // http://grepcode.com/file/repo1.maven.org/maven2/commons-io/commons-io/2.4/org/apache/commons/io/IOUtils.java
     public static boolean contentEquals(InputStream input1, InputStream input2) throws IOException {
-        if (!(input1 instanceof BufferedInputStream)) {
-            input1 = new BufferedInputStream(input1);
-        }
-        if (!(input2 instanceof BufferedInputStream)) {
-            input2 = new BufferedInputStream(input2);
-        }
+        BufferedInputStream buf1 = toBufferedInputStream(input1);
+        BufferedInputStream buf2 = toBufferedInputStream(input2);
 
-        int ch = input1.read();
+        int ch = buf1.read();
         while (EOF != ch) {
-            int ch2 = input2.read();
+            int ch2 = buf2.read();
             if (ch != ch2) {
                 return false;
             }
-            ch = input1.read();
+            ch = buf1.read();
         }
 
-        int ch2 = input2.read();
-        return ch2 == EOF;
+        return buf2.read() == EOF;
+    }
+
+    private static BufferedInputStream toBufferedInputStream(InputStream inputStream) {
+        if (inputStream instanceof BufferedInputStream) {
+            return (BufferedInputStream) inputStream;
+        }
+        return new BufferedInputStream(inputStream);
     }
 
     public static void closeQuietly(Closeable closeable) {
