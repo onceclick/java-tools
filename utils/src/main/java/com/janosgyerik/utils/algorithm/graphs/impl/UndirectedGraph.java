4b27b654e22e9c7476bd08bf9beaa471245a5c4d
Janos Gyerik <janos@kronos>
2017-02-18 13:49:27 +0100

    Add UndirectedGraph

---
+++ utils/src/main/java/com/janosgyerik/utils/algorithm/graphs/impl/UndirectedGraph.java
@@ -0,0 +1,60 @@
+package com.janosgyerik.utils.algorithm.graphs.impl;
+
+import com.janosgyerik.utils.algorithm.graphs.api.Graph;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class UndirectedGraph implements Graph {
+    private final Map<Integer, List<Integer>> map = new HashMap<>();
+
+    @Override
+    public void addEdge(int v, int w) {
+        map.computeIfAbsent(v, k -> new ArrayList<>()).add(w);
+        map.computeIfAbsent(w, k -> new ArrayList<>()).add(v);
+    }
+
+    @Override
+    public int vertexCount() {
+        return map.size();
+    }
+
+    @Override
+    public int edgeCount() {
+        return map.values().stream().mapToInt(List::size).sum() / 2;
+    }
+
+    @Override
+    public Collection<Integer> adj(int v) {
+        return Collections.unmodifiableCollection(map.get(v));
+    }
+
+    @Override
+    public String asString() {
+        if (map.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            int from = entry.getKey();
+            for (int to : entry.getValue()) {
+                sb.append(from).append('-').append(to).append(',');
+            }
+        }
+        return sb.substring(0, sb.length() - 1);
+    }
+
+    public static Graph fromString(String input) {
+        Graph g = new UndirectedGraph();
+        try (Scanner scanner = new Scanner(input)) {
+            scanner.useDelimiter(Pattern.compile("[-,\\s]"));
+            while (scanner.hasNext()) {
+                int from = scanner.nextInt();
+                int to = scanner.nextInt();
+                g.addEdge(from, to);
+            }
+        }
+        return g;
+    }
+}
